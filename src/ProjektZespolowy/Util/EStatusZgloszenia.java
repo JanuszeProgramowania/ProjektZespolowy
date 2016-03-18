45050af42d606b2f5a13d47f637785c0d635c5d5
tomaszjodko <tomaszjodko3@gmail.com>
2016-03-18 01:05:46 +0100

    enumy

---
+++ src/ProjektZespolowy/Util/EStatusZgloszenia.java
@@ -0,0 +1,8 @@
+package ProjektZespolowy.Util;
+
+/**
+ * Created by Tomasz Jodko on 2016-03-18.
+ */
+public enum EStatusZgloszenia {
+    OCZEKUJE,PRZYJETE,ODRZUCONE,ZAKONCZONE
+}
--- src/test/test.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-/**
- * Created by Mateusz on 2016-03-15.
- */
-public class test {
-}
