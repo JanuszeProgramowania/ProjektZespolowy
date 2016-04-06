27db81aeed1381d4b23b33be537c2ac8f290971f
tomaszjodko <tomaszjodko3@gmail.com>
2016-04-06 15:15:18 +0200

    encje, dto

--- src/main/java/com/janusze/ProjektZespolowy/Zalacznik/ob/ZalacznikOB.java
+++ src/main/java/com/janusze/ProjektZespolowy/Zalacznik/ob/ZalacznikOB.java
@@ -1,10 +1,12 @@
 package com.janusze.projektzespolowy.zalacznik.ob;
 
-import com.janusze.projektzespolowy.user.ob.UserOB;
 import com.janusze.projektzespolowy.binaria.ob.BinariaOB;
+import com.janusze.projektzespolowy.historia.ob.HistoriaOB;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.Date;
 
 /**
@@ -12,13 +14,34 @@ import java.util.Date;
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "zalaczniki")
+@SequenceGenerator(allocationSize = 1, name = "SEQ", sequenceName = "GEN_ZALACZNIK_ID")
 public class ZalacznikOB{
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ")
     private long id;
+    @Column(name = "DATA_UTWORZENIA", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date dataUtworzenia;
+    @Column(name = "DATA_MODYFIKACJI", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date dataModyfikacji;
+    @Column(name="MIME_TYPE")
     private String mimeType;
+    @Column(name="NAZWA")
     private String nazwa;
+    @OneToOne
     private BinariaOB binaria;
-    private UserOB user;
+    @ManyToOne
+    private HistoriaOB historia;
+
+    @PrePersist
+    @PreUpdate
+    private void setCurrentDate() {
+        dataModyfikacji = new Date();
+    }
 
 }
